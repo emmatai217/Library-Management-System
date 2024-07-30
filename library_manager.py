00161e0efe500de535ce3590ad314831e601873b
Emma Tai <mstaiyilun@gmail.com>
2024-07-30 20:44:08 +0800

    Initial commit: Add library manager module and README

---
+++ library_manager.py
@@ -0,0 +1,12 @@
+# library_manager.py
+
+def add_book(library, book):
+    library.append(book)
+    return library
+
+def remove_book(library, book):
+    library.remove(book)
+    return library
+
+def list_books(library):
+    return library
